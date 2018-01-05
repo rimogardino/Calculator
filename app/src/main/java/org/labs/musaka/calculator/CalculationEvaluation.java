9afafd3cd4ab9452504c55d802495723db72732b
rimogardino <tiddley@abv.bg>
2018-01-05 02:46:30 +0200

    Initial improvements, but still don't know if it calculates properly...

--- app/src/main/java/org/labs/musaka/calculator/CalculationEvaluation.java
+++ app/src/main/java/org/labs/musaka/calculator/CalculationEvaluation.java
@@ -9,37 +9,55 @@ import java.util.List;
  * Created by rimogardino on 1/2/18.
  */
 
+//to do's
+
+
+
+
 public final class CalculationEvaluation {
-    public static double result;
+    public static String result;
+    private static boolean defaultBadCase;
     private static ArrayList<String> reservedCarecters = new ArrayList<>();
+    public static final String erroneousCalculation = "Err!!";
 
     public static String evalStatement(String statement) {
         reservedCarecters.add("*");
         reservedCarecters.add("/");
         reservedCarecters.add("+");
         reservedCarecters.add("-");
-
+        defaultBadCase = false;
 
         Object[] tempSplit = operSplit(statement);
-        ArrayList<Double> operants = (ArrayList<Double>) tempSplit[0];
-        ArrayList<String> operations = (ArrayList<String>) tempSplit[1];
-        result = calculate(operants,operations);
+        ArrayList<String> operants = (ArrayList<String>) tempSplit[0];
+        //check if the operants represent a calculable thing else return an error
+        //attempt to calculate if the calculation goes properly return the result else return an error
+        if (isLegitStatement(operants)) {
+            result = calculate(operants);
+            if (defaultBadCase == true) {
+                return erroneousCalculation;
+            } else {
+                return String.valueOf(result);
+            }
+        } else {
+            return erroneousCalculation;
+        }
+        //ArrayList<String> operations = (ArrayList<String>) tempSplit[1];
 
-        return String.valueOf(result);
     }
 
-    private static double calculate(ArrayList<Double> operants, ArrayList<String> operations) {
+
+    private static String calculate(ArrayList<String> operants) {
 
         for (int i=0;i<reservedCarecters.size();i++) {
-            if (operations.contains(reservedCarecters.get(i))) {
-                int indexOfOperation = operations.indexOf(reservedCarecters.get(i));
-                result = subCalculate(new Double[]{operants.get(indexOfOperation),operants.get(indexOfOperation+1)},reservedCarecters.get(i));
+            if (operants.contains(reservedCarecters.get(i))) {
+                int indexOfOperation = operants.indexOf(reservedCarecters.get(i));
+                result = subCalculate(new String[]{operants.get(indexOfOperation - 1), operants.get(indexOfOperation + 1)}, reservedCarecters.get(i));
                 operants.remove(indexOfOperation+1);
                 operants.remove(indexOfOperation);
-                operations.remove(indexOfOperation);
-                operants.add(indexOfOperation,result);
+                operants.remove(indexOfOperation - 1);
+                operants.add(indexOfOperation - 1, result);
 
-                result = calculate(operants,operations);
+                result = calculate(operants);
             }
         }
 
@@ -48,58 +66,102 @@ public final class CalculationEvaluation {
     }
 
 
-    private static double subCalculate(Double[] operants, String operation) {
-        double subResult;
+    private static String subCalculate(String[] operants, String operation) {
+        double subResult = 0;
+
+        double operant1 = Double.valueOf(operants[0]);
+        double opernat2 = Double.valueOf(operants[1]);
 /*        if (operants.length < 2) {
             subResult = 0;
             return subResult;
         }*/
         switch (operation) {
             case "+":
-                subResult = operants[0] + operants[1];
-                break;
-            case "-":
-                subResult = operants[0] - operants[1];
+                subResult = operant1 + opernat2;
                 break;
             case "*":
-                subResult = operants[0] * operants[1];
+                subResult = operant1 * opernat2;
                 break;
             case "/":
-                subResult = operants[0] / operants[1];
+                subResult = operant1 / opernat2;
                 break;
             default:
-                subResult = 0;
+                defaultBadCase = true;
                 break;
 
         }
 
-        return subResult;
+        return String.valueOf(subResult);
     }
 
 
     @NonNull
     private static Object[] operSplit(String statement) {
-        ArrayList<Double> operants = new ArrayList<>();
-        ArrayList<String> operations = new ArrayList<>();
+        ArrayList<String> operants = new ArrayList<>();
+        //ArrayList<String> operations = new ArrayList<>();
 
         String buffer = "";
         for (int i=0;i<statement.length();i++) {
-            if (reservedCarecters.contains(String.valueOf(statement.charAt(i)))) {
-                operants.add(Double.valueOf(buffer));
-                buffer = "";
-                operations.add(String.valueOf(statement.charAt(i)));
-            } else{
+            //Deals with positive/negative sign at the first char input
+            if (i == 0) {
+                if (String.valueOf(statement.charAt(i)).equals("-")) {
+                    buffer = "-";
+                } else if (String.valueOf(statement.charAt(i)).equals("+")) {
+                    //basically do nothing as the default for all numbers is positive
+                    buffer = "";
+                } else if (reservedCarecters.contains(String.valueOf(statement.charAt(i)))) {
+                    buffer = "";
+                    operants.add(String.valueOf(statement.charAt(i)));
+                }
+            }
+            //for i>0 checks if the current char is an operation and adds the buffered number if so, also adds "-" as part of the numbers
+            else if (reservedCarecters.contains(String.valueOf(statement.charAt(i)))) {
+                if (!buffer.isEmpty()) operants.add(buffer);
+
+                if (String.valueOf(statement.charAt(i)).equals("-")) {
+                    buffer = "-";
+                    if (operants.size() > 0 && !reservedCarecters.contains(operants.get(operants.size() - 1)))
+                        operants.add(String.valueOf("+"));
+                } else {
+                    buffer = "";
+                    operants.add(String.valueOf(statement.charAt(i)));
+                }
+            }
+            //fills consecutive numbers/points in the buffer
+            else {
                 buffer = buffer + String.valueOf(statement.charAt(i));
             }
 
         }
-
+        //adds the last entry from the buffer
         if (!buffer.equals("")) {
-            operants.add(Double.valueOf(buffer));
+            operants.add(buffer);
         }
 
-        return new Object[] {operants,operations};
+        return new Object[]{operants};
     }
 
+
+    private static boolean isLegitStatement(ArrayList<String> operants) {
+        if (operants.size() < 2) return false; //Not enough things to make a calculation
+        if (reservedCarecters.contains(operants.get(0)))
+            return false; //Cannot begin a statement with an operation
+        for (int i = 0; i < operants.size(); i++) {
+            if (reservedCarecters.contains(operants.get(i))) {
+                if (i != 0 && (reservedCarecters.contains(operants.get(i - 1)) || reservedCarecters.contains(operants.get(i + 1)))) {
+                    return false;
+                }
+            } else if (operants.get(i).contains(".")) {
+                String operantI = operants.get(i);
+                int countPoints = 0;
+                for (int k = 0; k < operantI.length(); k++) {
+                    if (operantI.charAt(k) == '.') countPoints++;
+                    if (countPoints > 1) return false;
+                }
+            }
+        }
+
+        return true;
+    }
 
 }
--- app/src/main/java/org/labs/musaka/calculator/MainActivity.java
+++ app/src/main/java/org/labs/musaka/calculator/MainActivity.java
@@ -1,32 +1,43 @@
 package org.labs.musaka.calculator;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView calcDisplay;
-    private String calcText;
+//to do's
+//calkMe(View view) this should enter the inputs at the position of the cursor
 
+
+public class MainActivity extends AppCompatActivity {
+    private EditText calcDisplay;
+    private StringBuilder calcText;
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        calcDisplay = (TextView) findViewById(R.id.calc_display);
-        calcText = "";
+        calcDisplay = findViewById(R.id.calc_display);
+        calcDisplay.setShowSoftInputOnFocus(false);
+        calcText = new StringBuilder("");
     }
 
     public void calkMe(View view) {
+        //this should enter the inputs at the position of the cursor
+        int cursorPos = calcDisplay.getSelectionStart();
         Button button = (Button) view;
         if (calcText.equals("0")) {
-            calcText = "";
+            calcText.replace(0, calcText.length(), "");
         }
-
-        calcText = calcText + button.getText();
+        calcText = calcText.insert(cursorPos, button.getText());
         calcDisplay.setText(calcText);
+        calcDisplay.setSelection(cursorPos + 1);
 
         //Toast.makeText(this,CalculationEvaluation.result,Toast.LENGTH_LONG).show();
     }
@@ -34,22 +45,28 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void makeCorrection(View view) {
-        if (calcText.length() > 1) {
-            calcText = calcText.substring(0,calcText.length() - 1);
+        int cursorPos = calcDisplay.getSelectionStart();
+        if (cursorPos > 0 && cursorPos <= calcText.length()) {
+            calcText.deleteCharAt(cursorPos - 1);
+            calcDisplay.setText(calcText);
+            calcDisplay.setSelection(cursorPos - 1);
+        } else {
+            calcDisplay.setText(calcText);
         }
-        else {
-            calcText = "0";
-        }
-        calcDisplay.setText(calcText);
-
     }
 
     public void calcResult(View view) {
-        String TempcalcText = CalculationEvaluation.evalStatement(calcText);
-        if (!TempcalcText.isEmpty()) {
-            calcText = TempcalcText;
+        String TempcalcText = CalculationEvaluation.evalStatement(calcText.toString());
+        if (TempcalcText.equals(CalculationEvaluation.erroneousCalculation) || TempcalcText.isEmpty()) {
+            calcText.replace(0, calcText.length(), "");
+            calcDisplay.setText(CalculationEvaluation.erroneousCalculation);
+        } else if (TempcalcText.equals("null")) {
+            calcText.replace(0, calcText.length(), "");
+            calcDisplay.setText(CalculationEvaluation.erroneousCalculation);
+        } else if (!TempcalcText.isEmpty()) {
+            calcText.replace(0, calcText.length(), TempcalcText);
             calcDisplay.setText(TempcalcText);
         }
-        Toast.makeText(this,TempcalcText,Toast.LENGTH_LONG).show();
+        calcDisplay.setSelection(calcText.length());
     }
 }
